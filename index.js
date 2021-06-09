1b117c7a1ee3c6b6a40ac0601c6f795cf383bab5
aaron-might <aaronmighty7@gmail.com>
2021-06-08 20:45:38 -0400

    create  const inquirer, inquirer function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 
+
+const inquirer = require('inquirer');
+
+inquirer
+const renderHTML=((answers) => {
+        const heading = answers.name;
+        const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<h1>${heading}</h1> 
+<h2>${heading}</h2> 
+<h3>${heading}</h3> 
+
+</body>
+</html>
+`
 // TODO: Create an array of questions for user input
 const questions = [];
 
