d3a4cabeb96f616832ef97e1545b1eccbebe3a39
aaron-might <aaronmighty7@gmail.com>
2021-06-08 22:28:43 -0400

    write inquirer to generate questions

--- index.js
+++ index.js
@@ -3,100 +3,100 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO:inquirer to generate questions
-inquirer.prompt(
-    [
-        {
-            type:'input',
-            message: 'what is the project title',
-            name:'title',
-            //validate: property to check that the user provided a value
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-        },
-        {
-            type:'input',
-            message:'describe the project goals',
-            name:'description',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-
-        },
-        {
-            type:'input',
-            message:'how do you install your app',
-            name:'installation',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-            WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-        },
-        {
-            type:'input',
-            message:'how do you use your app',
-            name:'usage information',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-
-        },
-        {
-            type:'input',
-            message:'how do you test instructions',
-            name:'instructions',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-
-        },
-        {
-            type:'input',
-            message:'how can we contribute',
-            name:'contribution',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-
-        },
-        {
-            type:'input',
-            message:'linkedIn',
-            name:'linkedIn',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-
-        },
-        {
-            type:'input',
-            message:'what licence did you used',
-            name:'license',
-            choices:['the MIT License','The GPL License','Apache license','GNU license','N/A'],
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-        },
-        {
-            type:'input',
-            message:'Github username',
-            name:'git',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-        },
-        {
-            type:'input',
-            message:'E-mail:',
-            name:'email',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-        }
-        {
-            type:'input',
-            message:'credit:',
-            name:'email',
-            validate:(value)=>{if (value){return true}else {return 'i need a value to continue'}}
-        }
-
+inquirer.prompt(    [
+    {
+        type:'input',
+        message: 'what is the project title',
+        name:'title',
         
-    ]
+    },
+    {
+        type:'input',
+        message:'describe the project goals',
+        name:'description',
+        
+
+    },
+    {
+        type:'input',
+        message:'how do you install your app',
+        name:'installation',
+       
+        WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+    },
+    {
+        type:'input',
+        message:'how do you use your app',
+        name:'usage information',
+      
+
+    },
+    {
+        type:'input',
+        message:'how do you test instructions',
+        name:'instructions',
+      
+
+    },
+    {
+        type:'input',
+        message:'how can we contribute',
+        name:'contribution',
+        
+
+    },
+    {
+        type:'input',
+        message:'linkedIn',
+        name:'linkedIn',
+        
+
+    },
+    {
+        type:'input',
+        message:'what licence did you used',
+        name:'license',
+       
+    },
+    {
+        type:'input',
+        message:'Github username',
+        name:'git',
+        
+    },
+    {
+        type:'input',
+        message:'E-mail:',
+        name:'email',
+       
+    }
+    {
+        type:'input',
+        message:'credit:',
+        name:'email',
+        
+    }
+
+    
+]
 )
 
 .then(({
-    title,
-    description,
-    installation,
-    usage information, 
-    instructions,
-    linkedIn,
-    contribution,
-    license,
-    email,
-    credits,
+title,
+description,
+installation,
+usage information, 
+instructions,
+linkedIn,
+contribution,
+license,
+email,
+credits,
 
 })=>{
+
+//template to be used
+
 
 
 
@@ -108,25 +108,9 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const { title } = require('process');
 
-inquirer
-const renderHTML=((answers) => {
-        const heading = answers.name;
-        const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
-</head>
-<body>
-<h1>${heading}</h1> 
-<h2>${heading}</h2> 
-<h3>${heading}</h3> 
 
-</body>
-</html>
-`
+
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
